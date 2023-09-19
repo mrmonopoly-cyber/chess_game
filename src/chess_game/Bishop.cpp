e5b90752b6cf497e3e4ebd627cc3ff47bc2186c5
mrmonopoly-cyber <alberto.damo@gmail.com>
2023-09-19 23:35:49 +0200

    implemented Bishop, Rook, Queen, King(No castle). NOT TESTED

---
+++ src/chess_game/Bishop.cpp
@@ -0,0 +1,57 @@
+#include <cmath>
+#include <vector>
+#include "Chess_pieces.h"
+#include "include/chess_game/Chess_pieces.h"
+
+using namespace chess;
+
+Bishop::Bishop() : framework::Piece("Bishop")
+{
+    //ok to be empty
+}
+Bishop::~Bishop() 
+{
+    //ok to be empty
+}
+
+std::vector<struct framework::position> *Bishop::context_to_check(
+        framework::position &start_position, framework::position &end_position,
+        const unsigned int owner)const 
+{
+    const unsigned int diff_x = std::abs((int) end_position.x - (int)start_position.x);
+    const unsigned int diff_y = std::abs((int) end_position.y - (int)start_position.y);
+    int increment_x = -1;
+    int increment_y = -1;
+    if(diff_x > 0){
+        increment_x = 1;
+    }
+    if (diff_y > 0) {
+        increment_y = 1;
+    }
+    if(diagonal_check(start_position, end_position)){
+        std::vector<framework::position> *res = new std::vector<framework::position>(diff_x);
+        for(int i=0;i<diff_x;i++)
+        {
+            res->emplace_back(framework::position{start_position.x + (increment_x * i),start_position.y + (increment_y *i)});
+        }
+        return res;
+    }
+    return nullptr;
+
+}
+bool Bishop::valid_move(const struct framework::Board_cell * context_array,
+        std::vector<struct framework::position> *positions,
+        const unsigned int size)const 
+{
+    if(!context_array){
+        return false;
+    }
+
+    for(int i=0;i<size;i++){
+        if(!context_array[i].is_empty()){
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ src/chess_game/King.cpp
@@ -0,0 +1,66 @@
+#include <cmath>
+#include <vector>
+#include "Chess_pieces.h"
+#include "include/chess_game/Chess_pieces.h"
+
+using namespace chess;
+
+Queen::Queen() : framework::Piece("Queen")
+{
+    //ok to be empty
+}
+Queen::~Queen() 
+{
+    //ok to be empty
+}
+
+std::vector<struct framework::position> *Queen::context_to_check(
+        framework::position &start_position, framework::position &end_position,
+        const unsigned int owner)const 
+{
+    const unsigned int diff_x = std::abs((int) end_position.x - (int)start_position.x);
+    const unsigned int diff_y = std::abs((int) end_position.y - (int)start_position.y);
+    int increment_x = -1;
+    int increment_y = -1;
+    if(diff_x > 1 || diff_y > 1){
+        return nullptr;
+    }
+    if(diff_x > 0){
+        increment_x = 1;
+    }
+    if (diff_y > 0) {
+        increment_y = 1;
+    }
+    if(vertical_check(start_position, end_position)){
+        increment_x = 0;
+    }else if (horizontal_check(start_position,end_position)) {
+        increment_y = 0;
+    }else if(diagonal_check(start_position, end_position)){
+        //ok to be empty
+    }else {
+        return nullptr;
+    }
+
+    std::vector<framework::position> *res = new std::vector<framework::position>(diff_x);
+    for(int i=0;i<diff_x;i++)
+    {
+        res->emplace_back(framework::position{start_position.x + (increment_x * i),start_position.y + (increment_y *i)});
+    }
+    return res;
+}
+bool Queen::valid_move(const struct framework::Board_cell * context_array,
+        std::vector<struct framework::position> *positions,
+        const unsigned int size)const 
+{
+    if(!context_array){
+        return false;
+    }
+
+    for(int i=0;i<size;i++){
+        if(!context_array[i].is_empty()){
+            return false;
+        }
+    }
+
+    return true;
+}
